b6b6bd179bcba8d8b890477585e595d848e456d8
Aven Le Zhou <aven@artmachines.net>
2022-11-10 21:26:41 +0800

    update gaze with smooth

--- gaze/sketch.js
+++ gaze/sketch.js
@@ -1,31 +1,37 @@
 // Gaze, 2022
-// Aven Le ZHOU, https://aven.cc + Rong HUANG
+// Aven Le ZHOU, https://aven.cc 
 // CMA, HKUST(GZ)
-// for CMAA 6001
+// the idea and workflow are inspired by https://github.com/emilyxxie/mona_lisa_eyes 
 
 let video;
 let poseNet;
 let poses = [];
 
 let painting;
-let vid;
-
-let playing = false;
-let nose;
+let vid, vidLength;
+let loaded = false;
+let noseX, pnoseX;
 
 function preload() {
   painting = loadImage("gothic.jpeg");
-  painting.resize(1024, 1244);
 }
 
-function vidLoad() {
-  vid.loop();
-  vid.volume(0);
+function vidLoaded() {
+  vid.volume(0.3);
   vid.hide();
+  console.log("Video Loaded");
+  loaded = true;
+  vidLength = vid.duration();
+}
+
+function modelReady() {
+  console.log("Model Loaded");
 }
 
 function setup() {
-  createCanvas(1024, 1244 + 480);
+  createCanvas(1024, 1244);
+  
+  //posenet
   video = createCapture(VIDEO);
   video.size(640, 480);
   poseNet = ml5.poseNet(video, modelReady);
@@ -34,63 +40,50 @@ function setup() {
   });
   video.hide();
 
-  vid = createVideo(["animated-gothic.mp4"], vidLoad);
+  //animated video
+  vid = createVideo(["animated-gothic.mp4"], vidLoaded);
 
-  noStroke();
-}
-
-function modelReady() {
-  // select('#status').html('Model Loaded');
-  console.log("Model Loaded");
+  //static painting
+  image(painting, 0, 0, 1024, 1244);
+  
+  frameRate(24);
 }
 
 function draw() {
-  image(painting, 0, 0, 1024, 1244);
+  noseX = detectPos();
 
-  if (nose) {
-    let f = nose.position.x / 640;
-    f = constrain(f, 0,1);
-    vid.time(f * vid.duration());
-    // console.log(nose.position.x / 640);
-    let img = vid.get();
+  if (noseX && loaded) {
+    //smooth the data with noseX, pnoseX and lerp
+    let f = lerp(noseX, pnoseX, 0.01);
+    // console.log(f);
+    let img = findFrame(vid, vidLength, f);
     image(img, 399, 0, 625, 625);
   }
+
+  //draw video capture
   push();
-  translate(384, 1244);
-  image(video, 0, 0, 640, 480);
-  drawKeypoints();
+  translate(170, 1114);
+  scale(-1, 1);
+  image(video, 0, 0, 160, 120);
   pop();
 
-  rect(0, 1244, 384, 480);
+  pnoseX = noseX;
 }
 
-// A function to draw ellipses over the detected keypoints
-function drawKeypoints() {
+function findFrame(inputVideo, vLen, fPos) {
+  fPos = constrain(fPos, 0.01, 0.99);
+  inputVideo.time(fPos * vLen);
+  let img = inputVideo.get();
+  return img;
+}
+
+function detectPos() {
+  let nosePos;
   if (poses.length > 0) {
     let pose = poses[0].pose;
-    nose = pose.keypoints[0];
-    fill(255, 0, 0);
-    noStroke();
-    ellipse(nose.position.x, nose.position.y, 10, 10);
-  }
-}
-
-// A function to draw the skeletons
-function drawSkeleton() {
-  // Loop through all the skeletons detected
-  for (let i = 0; i < poses.length; i++) {
-    let skeleton = poses[i].skeleton;
-    // For every skeleton, loop through all body connections
-    for (let j = 0; j < skeleton.length; j++) {
-      let partA = skeleton[j][0];
-      let partB = skeleton[j][1];
-      stroke(255, 0, 0);
-      line(
-        partA.position.x,
-        partA.position.y,
-        partB.position.x,
-        partB.position.y
-      );
-    }
+    nosePos = pose.keypoints[0];
+    return nosePos.position.x / 640;
+  } else {
+    return null;
   }
 }
